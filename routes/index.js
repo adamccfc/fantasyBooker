0fd5a192626b0a38eba307a5e83c65ad28e6a817
Adam Morgan <adamccfc@gmail.com>
2016-12-18 13:52:55 +0000

    Added form to allow user submissions to db

--- routes/index.js
+++ routes/index.js
@@ -13,4 +13,39 @@ router.get('/', function(req, res, next) {
   });
 });
 
+/* GET New User page. */
+router.get('/add-match', function(req, res) {
+  res.render('add-match', { title: 'Add New Match' });
+});
+
+/* POST to Add User Service */
+router.post('/add-match', function(req, res) {
+  // Set our internal DB variable
+  var db = req.db;
+
+  // Get our form values. These rely on the "name" attributes
+  var stipulation = req.body.stipulation;
+  var event = req.body.event;
+  var timeLimit = req.body.timeLimit;
+
+  // Set our collection
+  var collection = db.get('fantasyBooker');
+
+  // Submit to the DB
+  collection.insert({
+    "stipulation" : stipulation,
+    "event" : event,
+    "timeLimit" : timeLimit
+  }, function (err, doc) {
+    if (err) {
+      // If it failed, return error
+      res.send("There was a problem adding the information to the database.");
+    }
+    else {
+      // And forward to success page
+      res.redirect("/");
+    }
+  });
+});
+
 module.exports = router;
