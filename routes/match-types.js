6e6ce6b75a3421692cf92f73f780244a585c766e
Adam Morgan <adamccfc@gmail.com>
2017-05-01 17:19:06 +0100

    Renamed match types to stipulation

--- routes/match-types.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET New User page. */
-router.get('/', function(req, res) {
-  res.render('match-types', { title: 'See different match types' });
-});
-
-module.exports = router;
---
+++ routes/stipulation.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET New User page. */
+router.get('/:stipulation', function(req, res) {
+
+  var db = req.db;
+  var collection = db.get('matches');
+
+  collection.find({stipulation: req.params.stipulation}, function(err, results) {
+
+    res.render('stipulation', {
+      'title': req.params.stipulation + " Matches",
+      'matches': results,
+      'json': JSON.stringify(results)
+    });
+  });
+});
+
+module.exports = router;
