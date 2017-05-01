f5b515b7b565f1a5eabf6497b2ce6336c89ab2f4
Adam Morgan <adamccfc@gmail.com>
2017-05-01 20:12:11 +0100

    Return stipulation matches by newest first

--- routes/stipulation.js
+++ routes/stipulation.js
@@ -7,7 +7,7 @@ router.get('/:stipulation', function(req, res) {
   var db = req.db;
   var collection = db.get('matches');
 
-  collection.find({stipulation: req.params.stipulation}, function(err, results) {
+  collection.find({stipulation: req.params.stipulation}, {sort: {date: -1}}, function(err, results) {
 
     res.render('stipulation', {
       'title': req.params.stipulation + " Matches",
